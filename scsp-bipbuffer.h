9144a101fd0e63035b9e27afa6f8fc378c970afe
Dave <dme2223@gmail.com>
2021-07-09 19:39:42 -0500

    wip

--- scsp-bipbuffer.h
+++ scsp-bipbuffer.h
@@ -21,6 +21,10 @@
  *   other consumes. And we can get consistancy from ensuring that we're not sending bad data (if we don't get the data in time
  *   should we send silence??) and we can potentially do that by blocking until we have at least the correct amount of data.
  *
+ * General Procedure:
+ *   Two seperate threads, in each;
+ *     a mutex for locking while reading/writing
+ *     a  semaphore for signaling
  *
  * e.g.
  *
@@ -32,29 +36,182 @@
  *   //read data and clear reserved region
  *   uint16_t* res_data = try_read_and_drop(b);   //this should be performed via the read thread
  *
+ *
  * */
 
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 
+//our pthreads for reading and writing to our bip-buffer
+typedef struct rw {
+  pthread_t write_thread;
+  pthread_t read_thread;
+} rw;
+
 typedef struct bip_buffer{
   uint32_t capacity;
   uint32_t readsize;
-  void* reserved_head; /* NB: reads will be from reserved_head to reserved_head+readsize */
+  uint32_t reserved_head; /* NB: reads will be from reserved_head to reserved_head+readsize */
+  uint32_t reserved_tail;
 
-  void* buffer;
+  uint16_t* buffer;
 
   //regions
   uint32_t a_head;
   uint32_t a_tail;
   uint32_t b_head;
   uint32_t b_tail;
+
+  sem_t* sem;
+
 } bip_buffer;
 
-bip_buffer* init_buffer();
+typedef struct threadargs{
+  bip_buffer *b;
+  void* data;
+  uint16_t rw_size;
+} threadargs;
 
-uint8_t* try_write_and_commit(bip_buffer* b, void* data);
 
+void* temp_read_fn(void* arg);
+
+int try_write_and_commit(threadargs* ta);
+
+/*
+rw* init_threads(){
+  rw* rw_threads = malloc(sizeof(rw));
+  pthread_create(&read_thread, NULL, temp_write_fn,NULL);
+
+  rw_threads->write_thread = write_thread;
+  rw_threads->read_thread = read_thread;
+
+  return rw_threads;
+}
+*/
+
+bip_buffer* init_buffer(uint32_t capacity, uint32_t readsize){
+  uint16_t* buff = calloc(0,capacity * sizeof(uint16_t));
+  bip_buffer* b = malloc(sizeof(bip_buffer));
+  sem_t* sem = malloc(sizeof(sem_t));
+  int ret = sem_init(sem,0,0);
+
+  if (ret){
+	exit(1);
+  }
+
+  b->capacity = capacity;
+  b->readsize = readsize;
+  b->reserved_head = 0;
+  b->reserved_tail = 0;
+  b->buffer = buff;
+  b->a_head = 0;
+  b->a_tail = 0;
+  b->b_head = 0;
+  b->b_tail = 0;
+  b->sem = sem;
+
+  return b;
+}
+
+uint32_t commit_min(uint32_t x, uint32_t y){
+  return x >= y ? y : x;
+}
+
+int16_t* read_data(bip_buffer* b){
+  int16_t* ret_buff = calloc(0, b->a_tail-b->a_head);
+
+  memcpy(&ret_buff, &b->buffer[b->a_head], b->readsize);
+  return ret_buff;
+}
+
+uint8_t reserve(bip_buffer* b){
+  uint32_t rsrv_head;
+  uint32_t free_space = b->b_head - b->a_head;
+
+  if (free_space > 0)
+	rsrv_head = b->b_tail;
+  else{
+	uint32_t a_space = b->capacity - b->a_tail;
+	if(a_space >= b->a_head){
+	  rsrv_head = b->a_head;
+	  free_space = a_space;
+	}else{
+	  free_space = b->a_head;
+	  rsrv_head = 0;
+	}
+  }
+  uint32_t rsrv_len = commit_min(free_space, b->capacity);
+  b->reserved_head = rsrv_head;
+  b->reserved_tail = rsrv_head + rsrv_len;
+  return 0;
+}
+
+//comit commit_size blocks on b returns 0 if successful, 1 otherwise
+uint8_t commit(bip_buffer* b, uint32_t commit_size){
+  //if commit length > reserved_space...go with amt of reserved_spae
+  uint32_t to_commit = commit_min(commit_size, b->reserved_head - b->reserved_tail);
+
+  if ((b->a_tail - b->a_head == 0) && (b->b_tail - b->b_head == 0)){
+	b->a_head = b->reserved_head;
+	b->a_tail = b->reserved_head + to_commit;
+  } else if (b->reserved_head == b->a_tail){
+	b->a_tail += to_commit;
+  }else
+	b->b_tail += to_commit;
+
+  b->reserved_head = 0;
+  b->reserved_tail = 0;
+  return 0;
+}
+
+uint8_t decommit(bip_buffer* b, uint32_t size){
+  if (size >= b->a_tail - b->a_head){
+	b->a_head = b->b_head;
+	b->a_tail = b-> b_tail;
+	b->b_head = 0;
+	b->b_tail = 0;
+  }
+  else
+	b->a_head +=size;
+
+  return 0;
+}
+
+/* write to buffer */
+uint8_t write_data(bip_buffer* b, void* data){
+  memcpy(&b->buffer[b->reserved_head], &data, b->reserved_tail * sizeof(uint16_t));
+  return 0;
+}
+
+int write_and_commit(bip_buffer* b, void* data){
+  threadargs* ta = malloc(sizeof(threadargs));
+  ta->b = b;
+  ta->data = data;
+  ta->rw_size = 1024;
+
+  pthread_t write_thread;
+  pthread_create(&write_thread, NULL, (void*)try_write_and_commit, ta);
+
+  return 0;
+}
+
+//reserve space -> write data -> commit
+int try_write_and_commit(threadargs* ta){
+
+  //sem wait?
+  //mutex lock?
+
+  reserve(ta->b);
+  write_data(ta->b,ta->data);
+  commit(ta->b, ta->rw_size);
+
+  sem_post(ta->b->sem);
+
+  return 0;
+}
+
+//read from reserved space (reserved_head + readsize) -> decommit -> return buffer
 void* try_read_and_drop(bip_buffer* b);
